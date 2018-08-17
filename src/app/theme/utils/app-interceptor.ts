a95fef2c8476341cf254bc23c519b5ce0ac7f9d8
Eli <elicloudh@gmail.com>
2018-08-18 01:48:42 +0300

    ARTEM-3 : categories & products apis

--- src/app/theme/utils/app-interceptor.ts
+++ src/app/theme/utils/app-interceptor.ts
@@ -4,25 +4,31 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
 
+import { environment } from 'environments/environment'
+
 @Injectable()
 export class AppInterceptor implements HttpInterceptor {
     constructor( private spinner: NgxSpinnerService) {}
-  
+
     intercept (req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         this.spinner.show();
-        
-        return next.handle(req).do((event: HttpEvent<any>) => {
-          if (event instanceof HttpResponse) {        
+
+        const local = "assets/data"
+        const url = req.url.indexOf(environment.apiUrl) > -1 ? req.url : `${environment.apiUrl}${req.url}`;
+        const apiReq = req.clone({ url: url });
+
+        return next.handle(req.url.indexOf(local) > -1 ? req : apiReq).do((event: HttpEvent<any>) => {
+          if (event instanceof HttpResponse) {
              this.spinner.hide();
           }
         }, (err: any) => {
           if (err instanceof HttpErrorResponse) {
-            const started = Date.now();            
+            const started = Date.now();
             const elapsed = Date.now() - started;
             console.log(`Request for ${req.urlWithParams} failed after ${elapsed} ms.`);
            // debugger;
           }
         })
-    }  
+    }
 }
