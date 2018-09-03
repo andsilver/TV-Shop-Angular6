146168e4afd9448029565325709484d82d72a820
Eli <elicloudh@gmail.com>
2018-09-03 13:02:41 +0300

    NGPLATTETV-59: hide brands filter when no brands

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -13,7 +13,10 @@ export const routes: Routes = [
         resolve: { data: InitStateService },
         children: [
             { path: '', loadChildren: 'app/pages/home/home.module#HomeModule' },
-            { path: 'account', loadChildren: 'app/pages/account/account.module#AccountModule', data: { breadcrumb: 'Account instellingen' } },
+            { path: 'account',
+              loadChildren: 'app/pages/account/account.module#AccountModule',
+              data: { breadcrumb: 'Account instellingen' }
+            },
             { path: 'compare', loadChildren: 'app/pages/compare/compare.module#CompareModule', data: { breadcrumb: 'Vergelijkingslijst' } },
             { path: 'wishlist', loadChildren: 'app/pages/wishlist/wishlist.module#WishlistModule', data: { breadcrumb: 'Wensenlijst' } },
             { path: 'cart', loadChildren: 'app/pages/cart/cart.module#CartModule', data: { breadcrumb: 'Winkelwagen' } },
@@ -21,7 +24,10 @@ export const routes: Routes = [
             { path: 'contact', loadChildren: 'app/pages/contact/contact.module#ContactModule', data: { breadcrumb: 'Contact' } },
             { path: 'sign-in', loadChildren: 'app/pages/sign-in/sign-in.module#SignInModule', data: { breadcrumb: 'Inloggen ' } },
             { path: 'brands', loadChildren: 'app/pages/brands/brands.module#BrandsModule', data: { breadcrumb: 'Merken' } },
-            { path: 'products', loadChildren: 'app/pages/products/products.module#ProductsModule', data: { breadcrumb: 'Gevonden producten' } }
+            { path: 'products',
+              loadChildren: 'app/pages/products/products.module#ProductsModule',
+              data: { breadcrumb: 'Gevonden producten' }
+            }
         ]
     },
     { path: '**', component: NotFoundComponent }
