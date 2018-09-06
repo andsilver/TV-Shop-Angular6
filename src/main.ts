29c163e6ce7ca1e1d8ede31d8f83de33477fc426
Eli <elicloudh@gmail.com>
2018-09-06 10:30:25 +0300

    disable console.log on production

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production) {
+if (!environment.production) {
   enableProdMode();
-  if ( window ) {
-    window.console.log = function() {};
-  }
+  console.log = () => { return; };
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
