16c82ce88020945d1345527f618ceb55acf8eb15
Eli <elicloudh@gmail.com>
2018-09-06 10:09:28 +0300

    disable console.log on production

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  if ( window ) {
+    window.console.log = function() {};
+  }
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
