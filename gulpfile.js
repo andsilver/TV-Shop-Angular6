0747fe1c68c06fa63c40b341ee02c5547bae5d73
Eli <elicloudh@gmail.com>
2018-09-12 10:09:39 +0300

    NGPLATTETV-114: improve performance

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
 const gulp = require('gulp');
 const gzip = require('gulp-gzip');
+const minify = require('gulp-minify-css');
+const clean = require('gulp-clean');
+const uglify = require('gulp-uglify-es').default;
 
 const [command, value] = (process.argv[3]) ? process.argv[3].split('='): '';
+const src = (value) ? `./dist/${value}` : `./dist`;
 
-gulp.task('compress', function() {
-
-  const src = (value) ? `./dist/${value}` : `./dist`;
-
-  gulp.src([`${src}/*.js`, `${src}/*.css`])
+gulp.task('pack-js', () => {	
+  return gulp.src([`${src}/*.js`])
+    .pipe(uglify())
     .pipe(gzip())
     .pipe(gulp.dest(`${src}`));
+});
+
+gulp.task('pack-css', () => {
+  return gulp.src([`${src}/*.css`])
+    .pipe(minify())
+    .pipe(gzip())
+    .pipe(gulp.dest(`${src}`));
+});
+ 
+gulp.task('clean', ['pack-js', 'pack-css'], () => {
+  return gulp.src([`${src}/*.js`, `${src}/*.css`], {read: false})
+    .pipe(clean());
+});
+
+gulp.task('default', ['clean'], () => {
 });
