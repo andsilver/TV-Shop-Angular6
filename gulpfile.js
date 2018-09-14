0e16eba9b1069ef63a1a8a526f7a1f8ab8d6d553
Eli <elicloudh@gmail.com>
2018-09-14 11:27:30 +0300

    NGPLATTETV-126: performance improve

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const gzip = require('gulp-gzip');
 const minify = require('gulp-minify-css');
 const clean = require('gulp-clean');
 const uglify = require('gulp-uglify-es').default;
+const htmlmin = require('gulp-htmlmin');
 
 const [command, value] = (process.argv[2]) ? process.argv[2].split('='): '';
 const src = (value) ? `./dist/${value}` : `./dist`;
@@ -19,14 +20,26 @@ gulp.task('pack-css', () => {
     .pipe(gulp.dest(`${src}`));
 });
 
-gulp.task('gzip', ['pack-js', 'pack-css'], () => {
+gulp.task('remove-index', () => {
+  gulp.src(`${src}/index.html`, {read: false})
+    .pipe(clean());
+});
+
+gulp.task('pack-main-html', ['remove-index'], () => {
+
+  return gulp.src(['src/main.html'])
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest(`${src}/index.html`));
+});
+
+gulp.task('gzip', ['pack-js', 'pack-css', 'pack-main-html'], () => {
   return gulp.src([`${src}/*.*`])
     .pipe(gzip({ gzipOptions: { level: 9 }, skipGrowingFiles : true }))
     .pipe(gulp.dest(`${src}`));
 });
  
 gulp.task('clean', ['gzip'], () => {
-  return gulp.src([`${src}/*.js`, `${src}/*.css`, `${src}/*.html`, `${src}/*.css`, `${src}/*.json`, `${src}/*.ico`], {read: false})
+  return gulp.src([`${src}/*.js`, `${src}/*.css`, `${src}/*.html`, `${src}/*.json`, `${src}/*.ico`], {read: false})
     .pipe(clean());
 });
 
