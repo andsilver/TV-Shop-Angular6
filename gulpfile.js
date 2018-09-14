1459582bc84e01727a4e40554be8494db49af147
Eli <elicloudh@gmail.com>
2018-09-14 14:02:32 +0300

    NGPLATTETV-126: performance improve

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const minify = require('gulp-minify-css');
 const clean = require('gulp-clean');
 const uglify = require('gulp-uglify-es').default;
 const htmlmin = require('gulp-htmlmin');
+const replace = require('gulp-string-replace');
 
 const [command, value] = (process.argv[2]) ? process.argv[2].split('='): '';
 const src = (value) ? `./dist/${value}` : `./dist`;
@@ -20,26 +21,22 @@ gulp.task('pack-css', () => {
     .pipe(gulp.dest(`${src}`));
 });
 
-gulp.task('remove-index', () => {
-  gulp.src(`${src}/index.html`, {read: false})
-    .pipe(clean());
+gulp.task('pack-index-html', () => {
+
+  return gulp.src([`${src}/index.html`])
+    .pipe(htmlmin({ collapseWhitespace: true, minifyJS: true }))
+    .pipe(replace(new RegExp('script ', 'g'), 'script async '))
+    .pipe(gulp.dest(`${src}`));
 });
 
-gulp.task('pack-main-html', ['remove-index'], () => {
-
-  return gulp.src(['src/main.html'])
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest(`${src}/index.html`));
-});
-
-gulp.task('gzip', ['pack-js', 'pack-css', 'pack-main-html'], () => {
-  return gulp.src([`${src}/*.*`])
+gulp.task('gzip', ['pack-js', 'pack-css', 'pack-index-html'], () => {
+  return gulp.src([`${src}/*.js`, `${src}/*.css`, `${src}/*.html`])
     .pipe(gzip({ gzipOptions: { level: 9 }, skipGrowingFiles : true }))
     .pipe(gulp.dest(`${src}`));
 });
- 
+
 gulp.task('clean', ['gzip'], () => {
-  return gulp.src([`${src}/*.js`, `${src}/*.css`, `${src}/*.html`, `${src}/*.json`, `${src}/*.ico`], {read: false})
+  return gulp.src([`${src}/*.js`, `${src}/*.css`, `${src}/*.html`], {read: false})
     .pipe(clean());
 });
 
