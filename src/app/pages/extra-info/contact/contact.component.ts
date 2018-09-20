085a269dc9634827ca743b6b5d2482eddddecea6
Eli <elicloudh@gmail.com>
2018-09-20 17:24:22 +0300

    NGPLATTETV-148: mock advice & contact page

---
+++ src/app/pages/extra-info/contact/contact.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { AppService } from 'app/app.service';
+import { ActivatedRoute, UrlSegment } from '@angular/router';
+import { ScrollToService } from 'ng2-scroll-to-el';
+
+import { Store } from '@ngrx/store';
+import { State } from 'app/store';
+import * as CrumbActions from 'app/store/actions/crumb-path.action';
+import * as mock from './mock.json';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit, OnDestroy {
+
+  @ViewChild('customer_service')
+  customerServiceElement: ElementRef;
+
+  subscriptions = [];
+  stores = [];
+  customerServices = [];
+
+  constructor(
+    private appService: AppService,
+    private route: ActivatedRoute,
+    private scrollTo: ScrollToService,
+    private store: Store<State>) { }
+
+  ngOnInit() {
+    this.subscriptions = [
+      this.appService.getStores().subscribe(res => {
+        console.log(res);
+        this.stores = res;
+      }),
+      this.route.url.subscribe((paths: UrlSegment[]) => {
+        if (paths.length > 1) {
+          setTimeout(() => this.scrollTo.scrollTo(this.customerServiceElement.nativeElement, 500), 500);
+          this.store.dispatch(new CrumbActions.SaveCrumbPath([
+            {
+              name: 'Advies & Contact',
+              permalink: `/extrainfo/advies-contact/klantenservice`,
+              static: true,
+              default_title: true
+            }
+          ]));
+        } else {
+          this.store.dispatch(new CrumbActions.SaveCrumbPath([
+            {
+              name: 'Advies & Contact',
+              permalink: `/extrainfo/advies-contact`,
+              static: true,
+              default_title: true
+            }
+          ]));
+        }
+      })
+    ];
+
+    this.customerServices = mock['data']['customer_services'];
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+
+}
---
+++ src/app/pages/extra-info/learning-center/learning-center.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-learning-center',
+  templateUrl: './learning-center.component.html',
+  styleUrls: ['./learning-center.component.scss']
+})
+export class LearningCenterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
