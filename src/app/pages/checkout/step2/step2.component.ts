5f54d657e301a6050ff2e3f488a7c570f3ab894a
Eli <elicloudh@gmail.com>
2018-10-08 17:59:18 +0300

    NGPLATTETV-233

--- src/app/pages/checkout/step2/step2.component.ts
+++ src/app/pages/checkout/step2/step2.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { CheckoutStep } from '../step';
 import { CheckoutService } from '../checkout.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { select } from '@ngrx/store';
 declare var imgix: any;
 
 @Component({
@@ -39,15 +40,50 @@ export class Step2Component extends CheckoutStep implements OnInit {
         console.log(res);
         this.widget = res;
 
+        let shipping_selected = '-';
+        let customers_delivery_time = '';
+        let payment = '';
+        let bank = '';
+        let card = '';
+
+        if (this.pickupMethod.input_selected) {
+
+          this.currentMethod = this.pickupMethod.input_value;
+          shipping_selected = '';
+          const selectedLocation = this.pickupMethod.pickup_location.find(l => l.input_selected );
+          shipping_selected = selectedLocation ? selectedLocation.input_value : shipping_selected;
+
+        } else if ( this.deliverMethod.input_selected ) {
+
+          this.currentMethod = this.deliverMethod.input_value;
+          shipping_selected = this.deliverMethod.input_value;
+
+          const selectedDeliveryTime = this.deliverMethod.available_delivery_dates.input_value.find(d => d.selected);
+          customers_delivery_time = selectedDeliveryTime ? selectedDeliveryTime.value : '';
+
+          const selectedPayment = this.deliverMethod.payments.find(p => p.input_selected);
+          payment = selectedPayment ? selectedPayment.input_value : '';
+
+          if ( selectedPayment.banks_list ) {
+            const selectedBank = selectedPayment.banks_list.input_value.find(b => b.selected);
+            bank = selectedBank ? selectedBank.value : '';
+          }
+
+          if ( selectedPayment.cards_list ) {
+            const selectedCard = selectedPayment.cards_list.input_value.find(b => b.selected);
+            card = selectedCard ? selectedCard.value : '';
+          }
+        }
+
         this.form = this.formBuilder.group({
-          shipping_selected: '-',
-          customers_delivery_time: '',
+          shipping_selected: shipping_selected,
+          customers_delivery_time: customers_delivery_time,
           delivery_on_sunday: '0',
           alt_customers_delivery_time: '',
           afhalen_parent: '',
-          payment: '',
-          issuer_bank: '',
-          creditcard_type: '',
+          payment: payment,
+          issuer_bank: bank,
+          creditcard_type: card,
           delivery_by_urgently: '0',
           have_delivery_time: '1'
         });
